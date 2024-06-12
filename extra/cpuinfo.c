d62839656d65eb5037a6046b0e0d9651d31c209b
khaalid <khaliidcaliy@gmail.com>
2024-06-12 15:33:36 +0300

    some features added in cpuinfo

--- extra/cpuinfo.c
+++ extra/cpuinfo.c
@@ -1,5 +1,33 @@
 #include "../main.h"
-
+#include <cpuid.h>
+void cpuid() {
+    /* i intentionally compared leaf 0 and 1 because this is the only leaf values is being worked 
+    it may be removed from the future */
+    for (int i=0;i<2;i++) {
+        if (i==0) {
+            unsigned int eax,ebx,ecx,edx;
+            __get_cpuid(i,&eax,&ebx,&ecx,&edx);
+            char vendor[13];
+            memcpy(vendor, &ebx, 4);
+            memcpy(vendor+4, &edx, 4);
+            memcpy(vendor+8, &ecx, 4);
+            vendor[12] = '\0'; // Null-terminate the string
+            printf("CPU vendor: %s\n", vendor);
+        } else{
+            unsigned int eax, ebx, ecx, edx;
+            // Call CPUID instruction to retrieve information
+            __get_cpuid(i, &eax, &ebx, &ecx, &edx);
+            char *feature_names[]= {"fpu","vme","de","pse","tsc","pae","mce","cx8","apic","VMX"};
+            printf("supported features: ");
+            for (int j = 0; j < sizeof(feature_names) / sizeof(feature_names[0]); j++) {
+                if ((edx >> j) & 1 && (j < 32 ? (ecx >> j) & 1 : (ecx >> (j - 32)) & 1)) {
+                    printf("%s ", feature_names[j]);
+                }
+            }
+            printf("\n\n");
+        }
+    }
+}
 int cpuinfo() {
         printf(ANSI_COLOR_YELLOW "getting processor information\n" ANSI_COLOR_RESET);
    // long total_time= system_info.uptime;
@@ -35,5 +63,7 @@ int cpuinfo() {
     }
     printf("Number of cores: %d\n", cores_count /2);
     printf("number of processors: %d\n",processors_count);
+    // now getting the vendor 
+    cpuid();
     return 0;
 }

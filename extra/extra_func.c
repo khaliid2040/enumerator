6f81181a549259a60136cf70a8d6f66bf50daf6a
khaalid <khaliidcaliy@gmail.com>
2024-07-09 14:45:22 +0300

    improvement in security function

--- extra/extra_func.c
+++ extra/extra_func.c
@@ -17,41 +17,6 @@ int process_file(char *path,char *filename) {
 
 }
 //for checking the Linux security Modules
-/*int LinuxSecurityModule() {
-    FILE *fp;
-    char buffer[SIZE];
-    
-    // Open the /sys/kernel/securitylsm file for reading
-    fp = fopen("/sys/kernel/security/lsm", "r");
-    if (fp == NULL) {
-        perror("Error opening file");
-        return 1;
-    }
-
-    // Read the contents of the file
-    if (fgets(buffer, SIZE, fp) != NULL) {
-        // Check if SELinux is being used
-        if (strstr(buffer, "selinux")) {
-            printf(ANSI_COLOR_GREEN "SELinux detected.\n" ANSI_COLOR_RESET);
-        } 
-        // Check if AppArmor is being used
-        else if (strstr(buffer, "apparmor")) {
-            printf(ANSI_COLOR_GREEN "AppArmor detected.\n" ANSI_COLOR_RESET);
-        } 
-        // If neither SELinux nor AppArmor is found, print unknown
-        else {
-            printf(ANSI_COLOR_RED "Unknown LSM is in use.\n" ANSI_COLOR_RESET);
-        }
-    } else {
-        printf(ANSI_COLOR_RED "Unable to determine the LSM.\n" ANSI_COLOR_RESET);
-    }
-
-    // Close the file
-    fclose(fp);
-
-    return 0;
-
-}*/
 void LinuxSecurityModule(void) {
     #ifdef APPARMOR
     if (aa_is_enabled()) {

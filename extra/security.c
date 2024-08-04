c61c460999530b865e68f31b96d92c883c6fea7e
khaalid <khaliidcaliy@gmail.com>
2024-08-04 11:57:23 +0300

    selinux fixes

--- extra/security.c
+++ extra/security.c
@@ -1,6 +1,54 @@
 #include "../main.h"
 #ifdef SELINUX_H
-#include <selinux/selinux.h>
+#include <selinux/selinux.h> 
+void selinux(void) {
+    if (is_selinux_enabled()) {
+        printf("SELinux: " ANSI_COLOR_GREEN "enabled\n" ANSI_COLOR_RESET);
+    } else {
+        printf("SELinux: " ANSI_COLOR_RED "disabled\n" ANSI_COLOR_RESET);
+    }
+
+    FILE *state_file, *mls_file;
+    char buffer[1024];
+
+    // Check SELinux enforce status
+    state_file = fopen("/sys/fs/selinux/enforce", "r");
+    if (state_file == NULL) {
+        perror("fopen /sys/fs/selinux/enforce");
+        return;
+    }
+
+    if (fgets(buffer, sizeof(buffer), state_file) != NULL) {
+        bool enforce = atoi(buffer);
+        if (enforce) {
+            printf("SELinux: " ANSI_COLOR_GREEN "enforce\n" ANSI_COLOR_RESET);
+        } else {
+            printf("SELinux: " ANSI_COLOR_YELLOW "permissive\n" ANSI_COLOR_RESET);
+        }
+    } else {
+        perror("fgets /sys/fs/selinux/enforce");
+    }
+    fclose(state_file);
+
+    // Check SELinux MLS status
+    mls_file = fopen("/sys/fs/selinux/mls", "r");
+    if (mls_file == NULL) {
+        perror("        fopen /sys/fs/selinux/mls");
+        return;
+    }
+
+    if (fgets(buffer, sizeof(buffer), mls_file) != NULL) {
+        bool mls_enabled = atoi(buffer);
+        if (mls_enabled) {
+            printf("SELinux: " ANSI_COLOR_GREEN "MLS enabled\n" ANSI_COLOR_RESET);
+        } else {
+            printf("SELinux: " ANSI_COLOR_YELLOW "MLS disabled\n" ANSI_COLOR_RESET);
+        }
+    } else {
+        perror("fgets /sys/fs/selinux/mls");
+    }
+    fclose(mls_file);
+}
 #elif APPARMOR_H
 #include <sys/apparmor.h>
 void apparmor(void) {
@@ -45,11 +93,7 @@ void LinuxSecurityModule(void) {
     #ifdef APPARMOR_H
     apparmor();
     #elif defined(SELINUX_H)
-    if (is_selinux_enabled()) {
-        printf("SELinux: " ANSI_COLOR_GREEN "enabled\n" ANSI_COLOR_RESET);
-    } else if(!(is_selinux_enabled())) {
-       printf("SELinux: " ANSI_COLOR_RED "disabled\n" ANSI_COLOR_RESET);
-    }
+    selinux();
     #else
     printf(ANSI_COLOR_RED "No LSM found\n" ANSI_COLOR_RESET);
     #endif  
